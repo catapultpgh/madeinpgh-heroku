742654003469d9659256515822a40949b5995a09
Greg Nicholas <greg.nicholas@gmail.com>
2013-05-28 10:24:38 -0400

    remove more traces of Philly

---
+++ setenv.sh
@@ -0,0 +1,20 @@
+export APP_SECRET="fp2qncrq3n984394gy9vn945ygwnxery9834gnc29g4g94934gq9"
+
+export S3_KEY="AKIAJBZAXNGO2WSHX6JA"
+export S3_SECRET="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"
+export S3_BUCKET="catapultpgh-madeinpgh"
+
+# #epittsburgh
+# export TWITTER_KEY="BWXi4wRaKp6DMLRj3wxAQ"
+# export TWITTER_SECRET="aky0IzXuzfNRCFDVxbkuHkyVTRvkbdceHUDECcpgTU"
+
+#madeinpgh
+export TWITTER_KEY="kT5fcOKtQ3xKJuadgNoyg"
+export TWITTER_SECRET="KOhUa7ejeeDLeKL53UFata5UQgEeF2FJdMJfGLWFWNs"
+
+
+# export FACEBOOK_KEY=""
+# export FACEBOOK_SECRET=""
+
+# export GITHUB_KEY
+# export GITHUB_SECRET
